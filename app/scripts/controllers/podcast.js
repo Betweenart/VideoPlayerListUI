86a3fb0052834eeb2950959869f790cea3e01cb1
Lukasz Oltarzewski <Lukasz Oltarzewski>
2016-06-24 05:04:05 +0100

    initial functionality of the list is done. need to write smarter position detection.

--- app/scripts/controllers/podcast.js
+++ app/scripts/controllers/podcast.js
@@ -13,8 +13,8 @@ angular.module('videoPlayerListUiApp')
 
     var feedUrl = 'http://rss.cnn.com/services/podcasting/sitroom/rss.xml';
 
-    $scope.podListButtonUpName = 'Scroll Up';
-    $scope.podListButtonDownName = 'Scroll Down';
+    $scope.podListButtonUpName = 'Move Up';
+    $scope.podListButtonDownName = 'Move Down';
     $scope.podCast = null;
 
     // get feed from wherever we need
@@ -28,10 +28,155 @@ angular.module('videoPlayerListUiApp')
         }
       }
       $log.debug($scope.podCast);
+
+      // initalize the ui class
+      setTimeout(function () {
+        videoListClass.init('podListButtonUp', 'podListButtonDown', 'podListVideos', $scope.podCast.entries, 'podVideoPlayer', 'podVideoDescription');
+      }, 1);
     });
 
-    $scope.activeVideoDescription = 'description of video here';
+    $scope.activeVideoDescription = 'short description';
 
-    // video list
-    // TODO UI movement
+    // define video list class
+    var videoListClass = {
+      init: function (buttonUpId, buttonDownId, listId, videoEntries, playerId) {
+        if (!buttonUpId || !buttonDownId || !listId) {
+          return console.warn('missing list id\'s');
+        }
+        this.entries = videoEntries || [];
+        this.buttonUp = $('#' + buttonUpId);
+        this.buttonDown = $('#' + buttonDownId);
+        this.podList = $('#' + listId);
+        this.videosList = this.podList.find('li');
+        this.firstElement = this.videosList[0];
+        this.elHeight = this.firstElement.offsetHeight;
+        this.visibleElements = 4;
+
+        this.playing = null;
+        this.player = $('#' + playerId);
+
+        this.selected = 0;
+        this.buttonUp.focus();
+        this.selectVideo(this.selected);
+        this.addListeners();
+      },
+      addListeners: function () {
+        var _this = this;
+        this.buttonUp.on('keydown', function (event) {
+          event.preventDefault();
+          console.log(event.keyCode);
+          switch (event.keyCode) {
+            case 13:
+              _this.playVideo();
+              break;
+            case 38:
+            case 32:
+              _this.moveListUp();
+              break;
+            case 40:
+              _this.buttonDown.focus();
+              break;
+            default:
+              break;
+          }
+        });
+
+        this.buttonDown.on('keydown', function (event) {
+          event.preventDefault();
+          console.log(event.keyCode);
+          switch (event.keyCode) {
+            case 13:
+              _this.playVideo();
+              break;
+            case 38:
+              _this.buttonUp.focus();
+              break;
+            case 40:
+            case 32:
+              _this.moveListDown();
+              break;
+            default:
+              break;
+          }
+        });
+
+      },
+      moveListUp: function () {
+        console.log('up', this.selected);
+        if (this.selected === 0) {
+          return console.info('first element');
+        }
+
+        if (this.selected === 1) {
+          this.firstElement.style.marginTop = '0px';
+        } else if (this.selected === this.videosList.length - 1) { // last element
+          this.firstElement.style.marginTop = (parseInt(this.firstElement.style.marginTop, 10) + this.elHeight / 3) + 'px';
+        } else if (this.selected === this.videosList.length - 2 || this.selected === this.videosList.length - 3) {
+          // only select up
+        } else {
+          this.firstElement.style.marginTop = (parseInt(this.firstElement.style.marginTop, 10) + this.elHeight) + 'px';
+        }
+
+        this.selectVideoUp();
+      },
+      moveListDown: function () {
+        console.log('down', this.selected);
+        if (this.selected === this.videosList.length - 1) { // last element
+          return console.info('last element');
+        }
+
+        if (this.selected === 0 || this.selected === 1 || this.selected === 2) {
+          //just select
+        } else if (this.selected === 3) {
+          this.firstElement.style.marginTop = -(this.elHeight + (this.elHeight / 3)) + 'px';
+        } else if (this.selected === this.videosList.length - 2) {
+          this.firstElement.style.marginTop = -((this.videosList.length - this.visibleElements) * this.elHeight) + this.elHeight / 3 + 'px';
+        } else {
+          this.firstElement.style.marginTop = (parseInt(this.firstElement.style.marginTop, 10) - this.elHeight) + 'px';
+        }
+        this.selectVideoDown();
+      },
+      selectVideo: function () {
+        if (this.videosList && this.videosList.length > 0) {
+          this.videosList[this.selected].classList.add('active');
+        }
+      },
+      deselectVideo: function () {
+        if (this.videosList && this.videosList.length > 0) {
+          this.videosList[this.selected].classList.remove('active');
+        }
+      },
+      selectVideoUp: function () {
+        this.deselectVideo();
+        this.selected -= 1;
+        this.selectVideo();
+      },
+      selectVideoDown: function () {
+        this.deselectVideo();
+        this.selected += 1;
+        this.selectVideo();
+      },
+      playVideo: function () {
+        console.log('playVideo');
+        if (this.playing === this.selected) {
+          if (this.player[0].paused) {
+            this.player[0].play();
+          } else {
+            this.player[0].pause();
+          }
+        } else {
+          this.player[0].src = this.entries[this.selected].mediaGroups[0].contents[0].url;
+          this.player[0].load();
+          this.player[0].play();
+          this.playing = this.selected;
+        }
+
+        this.updateDescription();
+      },
+      updateDescription: function () {
+        $scope.activeVideoDescription = this.entries[this.selected].contentSnippet;
+        $scope.$apply();
+      }
+    };
+
   });
